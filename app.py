773c59c0940648879679b81839bc153cf7afc530
Saamiya Ahsan <saamiyaa835@gmail.com>
2023-07-17 02:24:30 +0530

    done

--- app.py
+++ app.py
@@ -1,1 +1,80 @@
+from flask import render_template, session, Flask, redirect, url_for,request
+from flask_wtf import *
+from wtforms import *
+from wtforms.validators import *
+from impinfo import list_of_states, find_crop, find_secity
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'key'
 
+class infoform(FlaskForm):
+    temperature = DecimalField('Temperature: ', validators=[DataRequired()])
+    humidity = DecimalField('Humidity: ', validators=[DataRequired()])
+    ph = DecimalField('Ph: ', validators=[DataRequired()])
+    rainfall = DecimalField('Rainfall(mm): ', validators=[DataRequired()])
+    ChooseState = SelectField("State: ", choices= list_of_states)
+    submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email: ')
+    password = StringField('Password: ')
+    submit = SubmitField('Submit')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+    
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    form = infoform()
+    if form.validate_on_submit():
+        session['temperature'] = form.temperature.data
+        session['humidity'] = form.humidity.data
+        session['ph'] = form.ph.data
+        session['rainfall'] = form.rainfall.data
+        session['ChooseState'] = form.ChooseState.data
+       
+        y = find_crop(float(form.temperature.data),float(form.humidity.data), float(form.ph.data),float(form.rainfall.data))
+        session['find_crop']=str(y[0])
+        session['find_secity'] = find_secity(str(y[0]),form.ChooseState.data)
+        return redirect(url_for('answer'))
+        
+    return render_template('main.html',form=form)
+
+    
+@app.route('/answer')
+def answer():
+    return render_template('answer.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/success_log')
+def success_log():
+    return render_template('login.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+# @app.route('/success')
+# def success():
+#     return render_template('main.html')
+
+@app.route('/success')
+def success():
+    form = LoginForm()
+    if form.validate_on_submit():
+         session['email'] = form.email.data
+         session['password'] = form.password.data
+    return redirect(url_for('main'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- impinfo.py
+++ impinfo.py
@@ -1,1 +1,35 @@
+import pandas as pd
+import pickle
+
+df = pd.read_csv('Processed Data/save.csv')
+
+list_of_states = list(df.State_Name.unique())
+
+with open('model/tmodel', 'rb') as f:
+    model=pickle.load(f)
+
+def find_crop(a, b, c, d):
+    x = model.predict([[a, b, c, d]])
+    return x
+
+
+x = find_crop(20.130175,81.604873, 7.628473, 262.717340)
+
+print("Predicted crop: ", str(x[0]))
+
+
+
+
+def find_secity(a, b):
+    y = len(df[(df.Crop==a)&(df.State_Name==b)])
+    if y==0:
+        return f"The weather of the predicted {a} is not possible in the state {b}"
+
+    y = list(df[(df.Crop==a)&(df.State_Name==b)].District_Name)
+    str1 = ", "
+    secities=str1.join(y)
+    return f"You Should grow {a} in the districts of {secities} in the state of {b}"
+
+print(find_secity('rice', 'Madhya Pradesh'))
+
 
